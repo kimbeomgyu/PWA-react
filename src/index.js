92a37c078d7334e0557967c1982e4865643557f4
kimbeomgyu <qjarb1994@gmail.com>
2020-03-30 02:24:11 +0900

    initialize firebase

--- src/index.js
+++ src/index.js
@@ -3,7 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 require('dotenv').config();
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+try {
+  firebase.app();
+} catch (error) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 ReactDOM.render(
   <React.StrictMode>
