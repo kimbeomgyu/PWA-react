94daa1c9eebc6c78ebcc3e7e0bbd6b5765e4c1e3
kimbeomgyu <qjarb1994@gmail.com>
2020-03-30 02:31:23 +0900

    Add user card list

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
 import { Card, CardHeader } from '@material-ui/core';
 
 const Users = () => {
   const [users, setUsers] = useState(null);
 
+  useEffect(() => {
+    if (users === null) {
+      const db = firebase.firestore();
+      const snapshot = db.collection('users').get();
+      snapshot
+        .then(collection => collection.docs.map(doc => ({ id: doc.id, ...doc.data() })))
+        .then(setUsers)
+        .catch(err => console.log(err), setUsers([]));
+    }
+  }, [users]);
 
   return users === null ? (
     '... is loading'
   ) : (
     <div>
+      {users.map(({ id, name }) => (
+        <Card key={id}>
+          <CardHeader title={name} />
+        </Card>
+      ))}
     </div>
   );
 };
